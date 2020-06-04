e86384ce94c585338b0142077b67e8d80c8f4901
degski <degski@gmail.com>
2020-06-03 19:05:04 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,45 @@
+
+// MIT License
+//
+// Copyright (c) 2020 degski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+
+#include <iostream>
+
+// C++17 - flexible endian test.
+template<typename ValueType>
+constexpr int hi_byte_index ( ) noexcept {
+    unsigned int test = 1ul;
+    return ( int ) ( ( ( size_t ) char( test ) ) * ( sizeof ( ValueType ) - 1 ) );
+}
+
+constexpr bool is_little_endian = hi_byte_index<short> ( );
+
+int main ( ) {
+
+    std::cout << "is little endian? " << std::boolalpha << is_little_endian << '\n';
+
+    return EXIT_SUCCESS;
+}
